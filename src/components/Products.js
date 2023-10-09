d0ea5f4a7fa5ed8a942212fa214611a1e34e8614
wojtko <j.w2001@onet.pl>
2023-10-09 23:15:55 +0200

    Product component styling.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import matcha from "../assets/images/mockup5.png";
 
 function Products() {
   return (
     <Container>
       <Heading>Our Products</Heading>
       <Main>
-        <CategoryName>Tea</CategoryName>
-        <CategoryName>Accessories</CategoryName>
+        <Text>
+          <CategoryName>Tea</CategoryName>
+          <CategoryName>Accessory</CategoryName>
+        </Text>
+        <MainContent>
+          <Item>
+            <img src={matcha} alt=""></img>
+          </Item>
+          <Item>
+            <img src={matcha} alt=""></img>
+          </Item>
+          <Item>
+            <img src={matcha} alt=""></img>
+          </Item>
+          <Item>
+            <img src={matcha} alt=""></img>
+          </Item>
+        </MainContent>
       </Main>
     </Container>
   );
@@ -19,12 +36,45 @@ const Container = styled.div`
   padding: 64px 0;
 `;
 
-const Heading = styled.h2``;
+const Heading = styled.h2`
+  margin-bottom: 64px;
+`;
 
 const Main = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   gap: 24px;
 `;
 
-const CategoryName = styled.h3``;
+const Text = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 48px;
+`;
+
+const CategoryName = styled.h3`
+  cursor: pointer;
+
+  &:nth-child(2) {
+    opacity: 0.5;
+  }
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Item = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  border: 1px solid green;
+
+  img {
+    width: 100%;
+    height: 28vh;
+    object-fit: cover;
+  }
+`;
